a4777286cf1052c568e9977149b400f4cf6c337c
a.rozhnov <a.rozhnov@cityads.ru>
2015-03-17 12:13:40 +0300

    laravel 5 test

---
+++ selenium_tests/rozhnov_laravel5_first.py
@@ -0,0 +1,43 @@
+from setup import helper
+from setup import config
+from selenium.webdriver import DesiredCapabilities
+from selenium import webdriver
+# -*- coding: utf-8 -*-
+
+
+def setup_module():
+    global driver
+    global user
+    global hub
+    hub = config.hub
+    driver = webdriver.Remote(
+        command_executor=hub,
+        desired_capabilities=DesiredCapabilities.CHROME)
+    user = config.wm
+
+
+def teardown_function(function):
+    helper.screenshot(function, driver)
+
+
+def teardown_module():
+    driver.quit()
+
+
+def test_laravel():
+    driver.get("http://192.168.55.55/")
+    text = driver.find_element_by_xpath("//div[@class='title']").text
+    assert text == 'Laravel 5'
+
+
+
+
+
+
+
+
+
+
+
+
+
